36f698d89515522065aa3bb405fff379956af2da
Rail <rail7755@gmail.com>
2023-07-24 19:09:24 +0300

    add auth screen

--- lib/auth_widget.dart
+++ lib/auth_widget.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:themoviedb/Theme/app_button_style.dart';
 
 class AuthWidget extends StatefulWidget {
   const AuthWidget({super.key});
@@ -25,8 +26,6 @@ class _AuthWidgetState extends State<AuthWidget> {
 }
 
 class _HeaderWidget extends StatelessWidget {
-  const _HeaderWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
     const textStyle = TextStyle(
@@ -36,6 +35,7 @@ class _HeaderWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(height: 25),
           _FormWidget(),
@@ -44,11 +44,21 @@ class _HeaderWidget extends StatelessWidget {
             'In order to use the editing and rating capabilities of TMDb, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple.',
             style: textStyle,
           ),
+          SizedBox(height: 5),
+          TextButton(
+              onPressed: () {},
+              style: AppButtonStyle.linkButton,
+              child: Text('Register')),
           SizedBox(height: 25),
           Text(
             'If you signed up but didn`t get your verification email.',
             style: textStyle,
           ),
+          SizedBox(height: 5),
+          TextButton(
+              onPressed: () {},
+              style: AppButtonStyle.linkButton,
+              child: Text('Verify email')),
         ],
       ),
     );
@@ -56,25 +66,108 @@ class _HeaderWidget extends StatelessWidget {
 }
 
 class _FormWidget extends StatefulWidget {
-  const _FormWidget({super.key});
-
   @override
   State<_FormWidget> createState() => __FormWidgetState();
 }
 
 class __FormWidgetState extends State<_FormWidget> {
+  final _loginTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  String? errorText = null;
+
+  void _auth() {
+    final login = _loginTextController.text;
+    final password = _passwordTextController.text;
+    if (login == 'admin' && password == 'admin') {
+      errorText = null;
+      print('open app');
+    } else {
+      errorText = 'Не верный логин или пароль';
+      print('show error');
+    }
+    setState(() {});
+  }
+
+  void _resetPassword() {
+    print('reset password');
+  }
+
   @override
   Widget build(BuildContext context) {
     const textStyle = TextStyle(
       fontSize: 16,
-      color: Colors.black,
+      color: Color(0xFF212529),
     );
+    final color = Color(0xFF01B4E4);
+    final textFieldDecorator = InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      isCollapsed: true,
+    );
+    final errorText = this.errorText;
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('Username'),
-        TextField(),
-        Text('Password'),
-        TextField(),
+        if (errorText != null) ...[
+          Text(
+            errorText,
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.red,
+            ),
+          ),
+          SizedBox(height: 20)
+        ],
+        Text(
+          'Username',
+          style: textStyle,
+        ),
+        SizedBox(height: 5),
+        TextField(
+          controller: _loginTextController,
+          decoration: textFieldDecorator,
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Password',
+          style: textStyle,
+        ),
+        SizedBox(height: 5),
+        TextField(
+          controller: _passwordTextController,
+          decoration: textFieldDecorator,
+          obscureText: true,
+        ),
+        SizedBox(height: 25),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: _auth,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(color),
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 8,
+                  ),
+                ),
+              ),
+              child: Text('Login'),
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            TextButton(
+              onPressed: _resetPassword,
+              style: AppButtonStyle.linkButton,
+              child: Text('Reset Password'),
+            ),
+          ],
+        )
       ],
     );
   }
