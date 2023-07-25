c3175bf9d74b171921cdc2cbd6c4f372d16e7ff4
Rail <rail7755@gmail.com>
2023-07-25 10:34:41 +0300

    test commit 3

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -72,14 +72,14 @@ class _FormWidget extends StatefulWidget {
 }
 
 class __FormWidgetState extends State<_FormWidget> {
-  final _loginTextController = TextEditingController();
-  final _passwordTextController = TextEditingController();
+  final _loginTextController = TextEditingController(text: 'admin');
+  final _passwordTextController = TextEditingController(text: 'admin');
   String? errorText = null;
 
   void _auth() {
     final login = _loginTextController.text;
     final password = _passwordTextController.text;
-    if (login == 'a' && password == 'a') {
+    if (login == 'admin' && password == 'admin') {
       errorText = null;
 
       // Navigator.of(context).pushNamed('/main_screen');
